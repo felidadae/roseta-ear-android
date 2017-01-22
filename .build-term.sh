b939b4b561103b393f7a3f09388e55d7f6c8c9c6
Szymon Bugaj <felidadae@gmail.com>
2017-01-22 20:51:56 +0100

    Initial commit;

---
+++ .build-term.sh
@@ -0,0 +1,10 @@
+#rebuild tools
+function re {
+	./gradlew assembleDebug
+	./gradlew installDebug
+	# sudo apt-get install adb
+	adb install -r $(find . -name *.apk)
+}
+function openhere {
+	android-studio ../Rosetus
+}
