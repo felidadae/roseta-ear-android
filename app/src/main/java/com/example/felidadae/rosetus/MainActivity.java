e043d77e41768c8bd2bf3540681020fb584ae526
Szymon Bugaj <felidadae@gmail.com>
2017-01-26 09:44:24 +0100

    Snapshot

--- app/src/main/java/com/example/felidadae/rosetus/MainActivity.java
+++ app/src/main/java/com/example/felidadae/rosetus/MainActivity.java
@@ -3,7 +3,6 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +14,53 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+    public native String stringFromJNI();
+
+    //to set by library user; only setters
+    private float S=1; //scale
+    private int noteSize;     
+    private int minNoteSpace;
+    private int margin;
+    //to set by library; only getters
+    private int xN, yN;
+    private int realNoteSpaceX, realNoteSpaceY;
+    private View createNote(int x, int y) {
+        View note = new View(this);
+        note.setBackgroundColor(Color.BLACK);
+        note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fretboard_onclick(v);
+            }
+        });
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(noteSize, noteSize);
+        params.leftMargin = x;
+        params.topMargin  = y;
+        return note;
+    }
+    private void prepareFretboard(RelativeLayout fretboard) {
+        int width  = fretboard.getWidth();
+        int height = fretboard.getHeight();
+
+        //case on width and height
+
+		//Read from R.dimen
+        noteSize     = (int) (S*R.dimen.noteRadius);
+        minNoteSpace = (int) (S*R.dimen.minNoteSpace);
+        margin       = (int) (S*R.dimen.fretboardMargin);
+
+        xN = (int) ((width  - 2*margin + minNoteSpace) / (noteSize + minNoteSpace));
+        yN = (int) ((height - 2*margin + minNoteSpace) / (noteSize + minNoteSpace));
+        realNoteSpaceX = ((width - 2*margin)/xN);
+        realNoteSpaceY = ((height- 2*margin)/yN);
+
+        for (int ix = 0; ix < xN; ix++)
+            for (int iy = 0; iy < yN; iy++) {
+                int x =(int) (margin + ix*(1+realNoteSpaceX)*noteSize);
+                int y =(int) (margin + iy*(1+realNoteSpaceY)*noteSize);
+                fretboard.addView(createNote(x,y));
+            }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,36 +70,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RelativeLayout fretboard = (RelativeLayout) findViewById(R.id.activity_main);
-
-        View fretboard = (View) findViewById(R.id.fretboard);
-        // Example of a call to a native method
-        //TextView tv = (TextView) findViewById(R.id.sample_text);
-        //tv.setText(stringFromJNI());
+        prepareFretboard(fretboard);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    private int passiveColor=-1;
-    private int activeColor=Color.RED;
     public void fretboard_onclick(View view) {
-        int currentColor = ((ColorDrawable)view.getBackground()).getColor();
-        if (passiveColor == -1) {
-            passiveColor = currentColor;
-        }
-        int fromColor;
-        int toColor;
-        if (passiveColor == currentColor) {
-            fromColor = passiveColor;
+        int activeColor = R.color.noteActive;
+        int unactiveColor=R.color.noteUnactive;
+
+        int currentColor, fromColor, toColor;
+        currentColor = ((ColorDrawable)view.getBackground()).getColor();
+        if (unactiveColor == currentColor) {
+            fromColor = unactiveColor;
             toColor   = activeColor;
         }
         else {
             fromColor = activeColor;
-            toColor   = passiveColor;
+            toColor   = unactiveColor;
         }
+
         ObjectAnimator colorAnimation = ObjectAnimator.ofInt(view, "backgroundColor",
             fromColor, toColor);
         colorAnimation.setRepeatCount(0);
@@ -62,19 +96,5 @@ public class MainActivity extends AppCompatActivity {
         colorAnimation.setTarget(view);
         colorAnimation.setDuration(3000);
         colorAnimation.start();
-
-//        final View imageView = view;
-//        ViewPropertyAnimator viewPropertyAnimator = imageView.animate()
-//                .scaleX(0.5f)
-//                .scaleY(0.5f)
-//                .setDuration(1000)
-//                .setInterpolator(new LinearInterpolator())
-//                .setListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationEnd(Animator animator) {
-//                        imageView.setScaleX(1);
-//                        imageView.setScaleY(1);
-//                    }
-//                });
     }
 }
